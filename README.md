742b72ae27947a1517b55a34a824ffb82a816116
C. Johnson <feedback@radgeek.com>
2021-03-20 14:25:02 -0500

    Start with a basic WordPress plugin template.

---
+++ README.md
@@ -0,0 +1,9 @@
+Mother of All WordPress Plugins
+===============================
+This is a plugin for the WordPress publishing platform.
+
+* Code is mostly in PHP.
+* Class and interface definitions are in ./classes
+* Some frequently reused utility classes and functions are in ./externals/myphp
+* Assets for client-side display (images and other binaries, CSS, JS) are in ./assets
+* HTML or other output templates are in ./templates
---
+++ wp-plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Mother of All Plugins
+ *
+ * @package WPPluginPackage
+ * @author C. Johnson
+ * @copyright I would prefer not to.
+ * @license GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Mother Of All Plugins
+ * Plugin URI: https://github.com/radgeek/wp-plugin/
+ * Description: This is a plugin for the WordPress publishing platform.
+ * Version: 2021.0320
+ * Author: C. Johnson
+ * Author URI: https://github.com/radgeek/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: wp-plugin-plugin
+ */
+
+$pluginDir = dirname(__FILE__);
+require_once("${pluginDir}/externals/myphp/myphp.class.php");
